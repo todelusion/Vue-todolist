418e406f50d5d44917dfbe83caeffe68977ea18d
todelusion <todelusion@gmail.com>
2022-07-11 18:01:55 +0800

    regist_checked_noAPI_50%

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -11,8 +11,8 @@
             <label for="email" class="text-sm text-primary mb-1">
                 Email
             </label>
-            <p v-show="checkEmpty(a)" class="text-red-600 text-xs mb-1">必須輸入email</p>
-            <input type="email" required v-model="email" placeholder="請輸入Email" class="py-3 px-4 bg-white rounded-lg font-medium">
+            <p v-show="checkEmpty() == 'email'" class="text-red-600 text-xs mb-1">必須輸入email</p>
+            <input type="email" v-model.trim="user.email" placeholder="請輸入Email" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
     <li class="mb-4 w-full">
@@ -20,8 +20,8 @@
             <label for="name" class="text-sm text-primary mb-1">
                 您的暱稱
             </label>
-            <p v-show="checkEmpty(b)" class="text-red-600 text-xs mb-1">必須輸入暱稱</p>
-            <input type="text" placeholder="請輸入您的暱稱" class="py-3 px-4 bg-white rounded-lg font-medium">
+            <p v-show="checkEmpty() == 'nickname'" class="text-red-600 text-xs mb-1">必須輸入暱稱</p>
+            <input type="text" v-model.trim="user.nickname" placeholder="請輸入您的暱稱" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
     <li class="mb-4 w-full">
@@ -29,8 +29,8 @@
             <label for="password" class="flex items-center text-sm text-primary">
                 <p>Password</p>
             </label>
-                <p v-show="checkEmpty(c)" class="text-red-600 text-xs mb-1">必須輸入密碼</p>
-            <input type="password" required v-model="password" placeholder="請輸入密碼" class="py-3 px-4 bg-white rounded-lg font-medium">
+                <p v-show="checkEmpty() == 'password'" class="text-red-600 text-xs mb-1">必須輸入密碼</p>
+            <input type="password" v-model.trim="user.password" placeholder="請輸入密碼" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
     <li class="mb-6 w-full">
@@ -38,8 +38,8 @@
             <label for="password" class="text-sm text-primary mb-1">
                 Confirm the password
             </label>
-            <p v-show="checkEmpty(d)" class="text-red-600 text-xs mb-1">密碼不相同</p>
-            <input type="password" placeholder="請再次輸入密碼" class="py-3 px-4 bg-white rounded-lg font-medium">
+            <p v-show="checkEmpty() == 'passwordChecked'" class="text-red-600 text-xs mb-1">密碼不相同</p>
+            <input type="password" v-model.trim="passwordChecked" placeholder="請再次輸入密碼" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
     <li class="w-full flex justify-center -ml-4 mb-6">
@@ -53,23 +53,39 @@ import axios from 'axios'
 export default {
     data(){
         return{
-            email: '',
-            nickname: '',
-            password: '',
+            user: {
+                email: '',
+                nickname: '',
+                password: '',
+            },
             passwordChecked: ''
         }
     },
     methods: {
         regist(){
-            if(this.email && this.password ){
-                console.log(true)
+            if((this.user.email && this.user.nickname && this.user.password && this.passwordChecked) !== ''){
+                if(this.user.password !== this.passwordChecked){
+                    return
+                }
+                else {
+                    console.log('submit')
+                }
+            }else {
+                return
             }
         },
-        checkEmpty(a, b, c, d){
-            if(this.email){
-                return a = false
-            }else {
-                return a = true
+        checkEmpty(){
+            if(this.user.email == ''){
+                return 'email'
+            }
+            if(this.user.nickname == ''){
+                return 'nickname'
+            }
+            if(this.user.password == ''){
+                return 'password'
+            }
+            if(this.user.password === this.passwordChecked){
+                return 'passwordChecked'
             }
 
         }
