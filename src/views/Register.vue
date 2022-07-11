3e92512e2d583c12da3ead73388ca2167405cd6d
todelusion <todelusion.gmail.com>
2022-07-11 21:47:48 +0800

    regist_checked_noAPI_100%

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -11,7 +11,7 @@
             <label for="email" class="text-sm text-primary mb-1">
                 Email
             </label>
-            <p v-show="checkEmpty() == 'email'" class="text-red-600 text-xs mb-1">必須輸入email</p>
+            <p v-show="user.email == '' ? true : false" class="text-red-600 text-xs mb-1">必須輸入email</p>
             <input type="email" v-model.trim="user.email" placeholder="請輸入Email" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
@@ -20,7 +20,7 @@
             <label for="name" class="text-sm text-primary mb-1">
                 您的暱稱
             </label>
-            <p v-show="checkEmpty() == 'nickname'" class="text-red-600 text-xs mb-1">必須輸入暱稱</p>
+            <p v-show="user.nickname == '' ? true : false" class="text-red-600 text-xs mb-1">必須輸入暱稱</p>
             <input type="text" v-model.trim="user.nickname" placeholder="請輸入您的暱稱" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
@@ -29,7 +29,7 @@
             <label for="password" class="flex items-center text-sm text-primary">
                 <p>Password</p>
             </label>
-                <p v-show="checkEmpty() == 'password'" class="text-red-600 text-xs mb-1">必須輸入密碼</p>
+                <p v-show="user.password == '' ? true : false" class="text-red-600 text-xs mb-1">必須輸入密碼</p>
             <input type="password" v-model.trim="user.password" placeholder="請輸入密碼" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
@@ -38,7 +38,7 @@
             <label for="password" class="text-sm text-primary mb-1">
                 Confirm the password
             </label>
-            <p v-show="checkEmpty() == 'passwordChecked'" class="text-red-600 text-xs mb-1">密碼不相同</p>
+            <p v-show="user.password === passwordChecked ? false : true" class="text-red-600 text-xs mb-1">密碼不相同</p>
             <input type="password" v-model.trim="passwordChecked" placeholder="請再次輸入密碼" class="py-3 px-4 bg-white rounded-lg font-medium">
         </form>
     </li>
@@ -67,29 +67,12 @@ export default {
                 if(this.user.password !== this.passwordChecked){
                     return
                 }
-                else {
-                    console.log('submit')
-                }
-            }else {
+                console.log('submit') 
+            }else{
                 return
             }
-        },
-        checkEmpty(){
-            if(this.user.email == ''){
-                return 'email'
-            }
-            if(this.user.nickname == ''){
-                return 'nickname'
-            }
-            if(this.user.password == ''){
-                return 'password'
-            }
-            if(this.user.password === this.passwordChecked){
-                return 'passwordChecked'
-            }
 
-        }
-        
+        },
     }
 }
 </script>
